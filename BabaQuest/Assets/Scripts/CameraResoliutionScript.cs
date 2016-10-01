4203651286f730357a729aeab125773ba6c37a46
MortaPlyPly <gupijazaure@gmail.com>
2016-10-01 14:04:36 +0300

    fuck

--- BabaQuest/Assets/Scripts/CameraResoliutionScript.cs
+++ BabaQuest/Assets/Scripts/CameraResoliutionScript.cs
@@ -4,11 +4,18 @@ using System;
 using UnityEngine.SceneManagement;
 
 public class CameraResoliutionScript : MonoBehaviour {
-
-    public Canvas menius;
-    public GameObject hiddenMeniu;
-    public GameObject hiddenScene;
-    public GameObject meniuButton;
+    ///////OLD CANVAS PAR
+    //public Canvas menius;
+    ///////NEW CANVAS PAR
+    public GameObject canvasObj; //this is game obj with children: canvas(shitty) and event system
+    // canvasObj -> canvas & event system
+    //canvas -> hiddenMeniu, hiddenScene, meniuButton, fightMeniu;
+    public Canvas hiddenMeniu; //function in CameraControllScript
+    public Canvas hiddenScene; //function in CameraControllScript
+    public Canvas meniuButton; //function in CameraControllScript
+    public Canvas fightMeniu; //function in CharScript
+    Canvas c;
+    ///////
     public float speed = 100f;
     private Vector2 vector = new Vector2(1,0);
     public bool fight = false;
@@ -16,15 +23,21 @@ public class CameraResoliutionScript : MonoBehaviour {
     void Start()
     {
         Debug.Log("This is where MENIU button should start to show");
-        //Instantiate(menius);
+        //c = canvasObj.transform.GetComponent<Canvas>();//Find<Canvas>("GameMainCanvas");
+        /*Instantiate(menius);
         Instantiate(meniuButton);
-        meniuButton.transform.parent = menius.transform;
+        meniuButton.transform.parent = menius.transform;*/
+        //Instantiate(canvasObj);
+        Instantiate(meniuButton);
+        //meniuButton.transform.parent = c.transform;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("EnemySpawnPoint"))
         {
+            Instantiate(fightMeniu);
+            //fightMeniu.transform.parent = c.transform;
             fight = true;
         }
         if (other.gameObject.CompareTag("Stop"))
@@ -33,6 +46,8 @@ public class CameraResoliutionScript : MonoBehaviour {
             //hiddenScene.transform.position = new Vector2(240, 150);
             Debug.Log("This is where HIDDEN SCENE button should start to show");
             Instantiate(hiddenScene);
+            //hiddenScene.transform.parent = c.transform;
+
         }
         Destroy(other.gameObject);
     }
@@ -40,6 +55,11 @@ public class CameraResoliutionScript : MonoBehaviour {
     void Update()
     {
         gameObject.GetComponent<Rigidbody2D>().velocity = vector.normalized * speed * Time.deltaTime;
+    }
+
+    void EndFight() //dar niekur nekvieciams
+    {
+        Destroy(fightMeniu);
     }
 
     ////canvas buttons & etc
@@ -57,6 +77,7 @@ public class CameraResoliutionScript : MonoBehaviour {
     {
         Destroy(meniuButton);
         Instantiate(hiddenMeniu);
+        //hiddenMeniu.transform.parent = c.transform;
         speed = 0f;
         //char speed = 0f;
     }
@@ -65,6 +86,7 @@ public class CameraResoliutionScript : MonoBehaviour {
     {
         Destroy(hiddenMeniu);
         Instantiate(meniuButton);
+        //meniuButton.transform.parent = c.transform;
         speed = 100f;
         //char speed = 100f;6
     }
