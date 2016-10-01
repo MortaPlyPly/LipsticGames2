9180125b7d41c8fd81f6575f9242604e806ea915
Sensationxox <sensationxox@yahoo.com>
2016-10-01 12:31:25 +0300

    Revert "fixed normal[] bug"
    This reverts commit 4f4a5bc7b09f3193211c8d13c7e968864f53cec6.

--- BabaQuest/Assets/Scripts/Fighting Styles/Mixed.cs
+++ BabaQuest/Assets/Scripts/Fighting Styles/Mixed.cs
@@ -7,17 +7,14 @@ namespace Assets.Scripts.Fighting_Styles
 {
     class Mixed : AbstractFightingStyle
     {
-		public Mixed()
-		{
-			normal = new int[] { 40, 30, 30 };
-			fear1 = new int[] { 40, 30, 30 };
-			fear2 = new int[] { 30, 35, 35 };
-			fear3 = new int[] { 30, 35, 35 };
-			anger1 = new int[] { 50, 30, 20 };
-			anger2 = new int[] { 70, 20, 10 };
-			winningMood1 = new int[] { 70, 30, 0 };
-			winningMood2 = new int[] { 90, 10, 0 };
-			finishIt = new int[] { 90, 10, 0 };
-		}
-	}
+        public new int[] normal = { 40, 30, 30 };
+        public new int[] fear1 = { 40, 30, 30 };
+        public new int[] fear2 = { 30, 35, 35 };
+        public new int[] fear3 = { 30, 35, 35 };
+        public new int[] anger1 = { 50, 30, 20 };
+        public new int[] anger2 = { 70, 20, 10 };
+        public new int[] winningMood1 = { 70, 30, 0 };
+        public new int[] winningMood2 = { 90, 10, 0 };
+        public new int[] finishIt = { 90, 10, 0 };
+    }
 }
