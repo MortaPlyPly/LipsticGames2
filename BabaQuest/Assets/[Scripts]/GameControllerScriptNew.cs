da5d908e43376fb49311bf495958e4a17acf4efa
MortaPlyPly <gupijazaure@gmail.com>
2016-11-29 20:02:35 +0200

    background moving

---
+++ BabaQuest/Assets/[Scripts]/GameControllerScriptNew.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerScriptNew : MonoBehaviour
+{
+    public GameObject background; //background prefab
+    GameObject background1; //background gameobj
+    GameObject background2; //background gameobj
+    Vector3 backgroundPossition = new Vector3(18.2f, 0, 0);
+    public float backgroundSpeed = 0.01f; //backround moving speed script
+    private bool backgroundMove = true;
+
+	void Start ()
+    {
+        background1 = (GameObject)Instantiate(background, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+	
+	void Update ()
+    {
+        MoveBackground();
+    }
+
+    private void MoveBackground()
+    {
+        if (backgroundMove) //move background and spawn new next
+        {
+            background1.GetComponent<Transform>().position = //anim background1
+                new Vector2(background1.GetComponent<Transform>().position.x - backgroundSpeed,
+                background1.GetComponent<Transform>().position.y);
+            if (background1.GetComponent<Transform>().position.x < -0.3f && background1.GetComponent<Transform>().position.x > -0.35f) //spawn background2 when time
+            {
+                if(background2 != null)
+                {
+                    Destroy(background2.gameObject);
+                }
+                background2 = (GameObject)Instantiate(background, backgroundPossition, Quaternion.identity);
+            }
+            if (background2 != null)
+            {
+                background2.GetComponent<Transform>().position = //anim background2
+                new Vector2(background2.GetComponent<Transform>().position.x - backgroundSpeed,
+                background2.GetComponent<Transform>().position.y);
+                if (background2.GetComponent<Transform>().position.x < -0.3f && background2.GetComponent<Transform>().position.x > -0.35f) //spawn background1 when time
+                {
+                    Destroy(background1.gameObject);
+                    background1 = (GameObject)Instantiate(background, backgroundPossition, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
